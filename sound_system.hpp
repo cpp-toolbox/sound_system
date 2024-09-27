c9a69e8eb4c422d33c94d19eac53ef7d2894fe40
cuppajoeman <ccn@cuppajoeman.com>
2024-09-27 01:18:10 -0400

    style: reformat using cpp-toolbox style guide

--- sound_system.hpp
+++ sound_system.hpp
@@ -1,25 +1,26 @@
 #ifndef OPENAL_MWE_SOUND_SYSTEM_HPP
 #define OPENAL_MWE_SOUND_SYSTEM_HPP
 
-#include "al.h"
+#include <AL/al.h>
 #include <map>
 #include <string>
 
 class SoundSystem {
-public:
+  public:
     SoundSystem();
     ~SoundSystem();
-    void load_sound_into_system_for_playback(const std::string& sound_name, const char *filename);
-    void create_sound_source(const std::string& source_name);
+    void load_sound_into_system_for_playback(const std::string &sound_name, const char *filename);
+    void create_sound_source(const std::string &source_name);
     void set_source_gain(const std::string &source_name, float gain);
     void set_source_looping_option(const std::string &source_name, bool looping);
-    void play_sound(const std::string& source_name, const std::string& sound_name);
+    void play_sound(const std::string &source_name, const std::string &sound_name);
     void set_listener_position(float x, float y, float z);
-private:
+
+  private:
     std::map<std::string, ALuint> sound_name_to_loaded_buffer;
     std::map<std::string, ALuint> source_name_to_source_id;
     void initialize_openal();
     void deinitialize_openal();
 };
 
-#endif //OPENAL_MWE_SOUND_SYSTEM_HPP
+#endif // OPENAL_MWE_SOUND_SYSTEM_HPP
